d18fddb160bdb94a2301a2a953fd96b6946c3ea8
sot528 <sot528@gmail.com>
2017-10-28 13:56:32 +0900

    add thawing validation.

--- contracts/FrozenWallet.sol
+++ contracts/FrozenWallet.sol
@@ -18,4 +18,21 @@ contract FrozenWallet is MultiSigWallet {
   {
     thawingTime = _thawingTime;
   }
+
+  /// overriding MultiSigWallet#submitTransaction
+  /// - To frozen until thawingTime.
+  ///
+  /// @dev Allows an owner to submit and confirm a transaction.
+  /// @param destination Transaction target address.
+  /// @param value Transaction ether value.
+  /// @param data Transaction data payload.
+  /// @return Returns transaction ID.
+  function submitTransaction(address destination, uint value, bytes data)
+  public
+  returns (uint transactionId)
+  {
+    require(now >= thawingTime);
+
+    return super.submitTransaction(destination, value, data);
+  }
 }

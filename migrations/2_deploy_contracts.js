59dfbdefaf060449329ec391c453e3cb3cbf9147
sot528 <sot528@gmail.com>
2017-10-28 16:03:16 +0900

    fix linting point.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,8 @@ const FrozenWallet = artifacts.require('FrozenWallet.sol');
 const walletParams = JSON.parse(fs.readFileSync('../config/FrozenWallet.json', 'utf8'));
 
 module.exports = function deployContracts(deployer) {
-  deployer.deploy(FrozenWallet, walletParams.owners, walletParams.required,
-    walletParams.thawingTime);
+  deployer.deploy(
+    FrozenWallet, walletParams.owners, walletParams.required,
+    walletParams.thawingTime,
+  );
 };
